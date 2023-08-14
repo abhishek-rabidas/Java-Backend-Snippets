6401ee14b7db5c8329e0ee9b616ce75c26e6ce54
abhishek-rabidas <avisec07@gmail.com>
2023-08-14 13:34:34 +0530

    project init

---
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/JavabackendsnippetsApplication.java
@@ -0,0 +1,13 @@
+package com.javabackendsnippets.github.javabackendsnippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabackendsnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabackendsnippetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javabackendsnippets/github/javabackendsnippets/JavabackendsnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabackendsnippets.github.javabackendsnippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabackendsnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
