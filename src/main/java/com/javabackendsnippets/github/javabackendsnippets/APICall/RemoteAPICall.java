03af52d2018941ebaed3136d4d48003c44c8c18e
abhishek-rabidas <avisec07@gmail.com>
2023-08-14 13:37:40 +0530

    remote api call

---
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/APICall/RemoteAPICall.java
@@ -0,0 +1,9 @@
+package com.javabackendsnippets.github.javabackendsnippets.APICall;
+
+
+public class RemoteAPICall {
+
+    public void doRemoteAPICall(String apiURL, String JSONBody) {
+
+    }
+}
