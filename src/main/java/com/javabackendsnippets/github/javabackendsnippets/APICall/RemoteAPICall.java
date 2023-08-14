58bd2675989ac71d3520721a36227f4db10df214
abhishek-rabidas <avisec07@gmail.com>
2023-08-14 13:43:25 +0530

    okttp service code

--- src/main/java/com/javabackendsnippets/github/javabackendsnippets/APICall/RemoteAPICall.java
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/APICall/RemoteAPICall.java
@@ -1,9 +1,27 @@
 package com.javabackendsnippets.github.javabackendsnippets.APICall;
 
+import okhttp3.*;
+
+import java.io.IOException;
 
 public class RemoteAPICall {
 
-    public void doRemoteAPICall(String apiURL, String JSONBody) {
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
+    public void doRemoteAPICall(String apiURL, String JSONBody) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSONBody, JSON);
+
+        Request request = new Request.Builder()
+                .url(apiURL)
+                .post(body)
+                .build();
+
+        try (Response response = okHttpClient.newCall(request).execute()) {
+            System.out.println(response.body().string());
+        } catch (IOException exception) {
+            exception.getStackTrace();
+        }
     }
 }
