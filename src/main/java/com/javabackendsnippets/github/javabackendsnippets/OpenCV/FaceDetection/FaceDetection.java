922ae67b60c2c6e2af22c54fe773be93abf1ffbc
abhishek-rabidas <avisec07@gmail.com>
2023-08-17 17:14:53 +0530

    cascade model add

--- src/main/java/com/javabackendsnippets/github/javabackendsnippets/OpenCV/FaceDetection/FaceDetection.java
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/OpenCV/FaceDetection/FaceDetection.java
@@ -1,5 +1,11 @@
 package com.javabackendsnippets.github.javabackendsnippets.OpenCV.FaceDetection;
 
+import org.opencv.objdetect.CascadeClassifier;
+
 public class FaceDetection {
 
+    public void storeFacesFromImage(String imageURL) {
+
+        CascadeClassifier classifier = new CascadeClassifier("src/main/resources/model/haarcascade_frontalface_default.xml");
+    }
 }
