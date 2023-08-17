c1671efef976200d9805464d7588c27779111357
abhishek-rabidas <avisec07@gmail.com>
2023-08-17 17:26:26 +0530

    face detection code

--- src/main/java/com/javabackendsnippets/github/javabackendsnippets/OpenCV/FaceDetection/FaceDetection.java
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/OpenCV/FaceDetection/FaceDetection.java
@@ -1,5 +1,10 @@
 package com.javabackendsnippets.github.javabackendsnippets.OpenCV.FaceDetection;
 
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 public class FaceDetection {
@@ -7,5 +12,36 @@ public class FaceDetection {
     public void storeFacesFromImage(String imageURL) {
 
         CascadeClassifier classifier = new CascadeClassifier("src/main/resources/model/haarcascade_frontalface_default.xml");
+
+        Mat image = Imgcodecs.imread(imageURL);
+
+        if (image.empty()) {
+            System.out.println("Empty image");
+            return;
+        }
+
+        Mat grayImage = new Mat();
+        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
+
+        MatOfRect faces = new MatOfRect();
+        classifier.detectMultiScale(grayImage, faces);
+
+        if (faces.empty()) {
+            System.out.println("Faces not found");
+            return;
+        }
+
+        int padding = 20, i = 0;
+
+        for (Rect rect : faces.toArray()) {
+            int x = Math.max(rect.x - padding, 0);
+            int y = Math.max(rect.y - padding, 0);
+            int width = Math.min(rect.width + 2 * padding, image.cols() - x);
+            int height = Math.min(rect.height + 2 * padding, image.rows() - y);
+
+            Mat faceROI = new Mat(image, new Rect(x, y, width, height));
+
+            Imgcodecs.imwrite("face_" + (++i) + ".jpg", faceROI);
+        }
     }
 }
