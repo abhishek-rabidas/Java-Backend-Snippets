310934e4d4fd74b3dae4c592908c23218ee506bf
abhishek-rabidas <avisec07@gmail.com>
2023-08-17 17:12:25 +0530

    opencv initializer code with try catch

--- src/main/java/com/javabackendsnippets/github/javabackendsnippets/OpenCV/LoadOpenCV.java
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/OpenCV/LoadOpenCV.java
@@ -1,11 +1,17 @@
 package com.javabackendsnippets.github.javabackendsnippets.OpenCV;
 
 import jakarta.annotation.PostConstruct;
+import nu.pattern.OpenCV;
 
 public class LoadOpenCV {
 
     @PostConstruct
     public void loadOpenCV() {
+        try {
+            OpenCV.loadLocally();
+        } catch (Exception e) {
+            System.out.println("Failed loading OpenCV");
+        }
 
     }
 }
