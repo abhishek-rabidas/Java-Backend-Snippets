bb69788bb89a349a834c648b015c2a58d7fb616c
abhishek-rabidas <avisec07@gmail.com>
2023-08-17 17:11:24 +0530

    opencv initializer

---
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/OpenCV/LoadOpenCV.java
@@ -0,0 +1,11 @@
+package com.javabackendsnippets.github.javabackendsnippets.OpenCV;
+
+import jakarta.annotation.PostConstruct;
+
+public class LoadOpenCV {
+
+    @PostConstruct
+    public void loadOpenCV() {
+
+    }
+}
