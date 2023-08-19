5560a935c4d08e4847ad8058deeeab133f4af638
abhishek-rabidas <avisec07@gmail.com>
2023-08-19 22:48:30 +0530

    cli service for echoing

--- src/main/java/com/javabackendsnippets/github/javabackendsnippets/CommandLineRunner/CommandLineRunnerService.java
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/CommandLineRunner/CommandLineRunnerService.java
@@ -2,8 +2,44 @@ package com.javabackendsnippets.github.javabackendsnippets.CommandLineRunner;
 
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 @Service
 public class CommandLineRunnerService {
 
+    public static void getTimeFromCLI() {
 
+        String command = "cmd.exe /c echo Hello, World!";
+
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(command);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        int exitCode = 0;
+        try {
+            exitCode = process.waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        InputStream inputStream = process.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line;
+        while (true) {
+            try {
+                if ((line = reader.readLine()) == null) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(line);
+        }
+
+        System.out.println("Exit Code: " + exitCode);
+    }
 }
--- src/main/java/com/javabackendsnippets/github/javabackendsnippets/JavabackendsnippetsApplication.java
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/JavabackendsnippetsApplication.java
@@ -1,5 +1,6 @@
 package com.javabackendsnippets.github.javabackendsnippets;
 
+import com.javabackendsnippets.github.javabackendsnippets.CommandLineRunner.CommandLineRunnerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavabackendsnippetsApplication {
 
 	public static void main(String[] args) {
+		CommandLineRunnerService.getTimeFromCLI();
 		SpringApplication.run(JavabackendsnippetsApplication.class, args);
 	}
 
