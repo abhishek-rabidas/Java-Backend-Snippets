0393c5725f1c67e30c3a87a1461aac0060f8741b
abhishek-rabidas <avisec07@gmail.com>
2023-08-19 22:22:29 +0530

    cli service init

--- src/main/java/com/javabackendsnippets/github/javabackendsnippets/CommandLineRunner/CommandLineRunnerService.java
+++ src/main/java/com/javabackendsnippets/github/javabackendsnippets/CommandLineRunner/CommandLineRunnerService.java
@@ -1,4 +1,9 @@
 package com.javabackendsnippets.github.javabackendsnippets.CommandLineRunner;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommandLineRunnerService {
+
+
 }
